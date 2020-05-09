0fe215919594afb1cf329a5ee291a220407e983e
intelon67 <ex0tic67@gmail.com>
2020-05-09 16:14:19 -0400

    modified:   week1/practioner1.php

--- week1/practioner1.php
+++ week1/practioner1.php
@@ -9,23 +9,9 @@
 		<h1>SE 266 - Jake Desmarais</h1>
        
        <a href="https://github.com/intelon67/SE266" style="font-size:24px;">My Github Repo</a>
-        <?php
 
-            $animals = animals('Giraffe'=>'Donkey'=>'Zebra'=>'Hippo'=>'Gorilla');
-            $animals = animals_values($animals);
 
-            
-            echo "<ul>"
-            echo "<li>$animals[0]</li>"
-            echo "<li>$animals[1]</li>"
-            echo "<li>$animals[2]</li>"
-            echo "<li>$animals[3]</li>"
-            echo "<li>$animals[4]</li>"
-            echo "</ul>"
-
-        ?>
-
-        <p>Website Modified: 5/9/2020 at 4:08PM EST</p>
+        <p>Website Modified: 5/9/2020 at PM EST</p>
 
             
     
