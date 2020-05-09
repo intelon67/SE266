6a194b89c7a0cdcb330bc03077042bc198a0af38
intelon67 <ex0tic67@gmail.com>
2020-05-09 18:23:38 -0400

    modified:   week1/age.php

--- week1/age.php
+++ week1/age.php
@@ -6,11 +6,9 @@ $age = intval($_POST["age"]);
 
 function ageVerf($age){
     if ($age < 21) {
-        echo "Your Age: " $age;
         echo "You are not old enough.";
     }
     else {
-        echo "Your Age: " $age;
         echo "You are old enough.";
     }
 }
