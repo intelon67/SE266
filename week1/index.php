0854e9ccf30f013e00d28b0730219cf2bdedcd66
intelon67 <ex0tic67@gmail.com>
2020-05-09 15:07:26 -0400

    modified:   debug.log 	modified:   week1/index.php

--- week1/index.php
+++ week1/index.php
@@ -2,14 +2,32 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>SE 266 - Hello World</title>
+        <title>SE 266 - Jake Desmarais</title>
     </head>
     <body>
-		<h1>SE 266 - Hello World</h1>
+		<h1>SE 266 - Jake Desmarais</h1>
        
        <?php
-            echo "Welcome to PHP. PHP rocks!";
+            echo "Git hub repo: https://github.com/intelon67/SE266";
+        ?>
+
+        <h2>PHP Resources</h2>
+        <?php
+            echo "https://www.w3schools.com/php/";
+            echo "https://www.tutorialspoint.com/php/index.htm";
+            echo "https://www.php.net/manual/en/tutorial.php";
+        ?>
+
+        <h2>Git Resources</h2>
+        <?php
+            echo "https://guides.github.com/introduction/git-handbook/";
+            echo "https://github.github.com/training-kit/";
+            echo "https://lab.github.com/";
         ?>
        
+       <h2>Things I Enjoy</h2>
+       <?php
+            echo "https://www.twi-global.com/technical-knowledge/faqs/what-is-welding";
+            echo "";
     </body>
 </html>
