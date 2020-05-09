cdfce5f15cc38caf854cc645dd31ad0b659c72dc
intelon67 <ex0tic67@gmail.com>
2020-05-09 19:01:24 -0400

    modified:   week1/fizzbuzz.php

--- week1/fizzbuzz.php
+++ week1/fizzbuzz.php
@@ -15,10 +15,8 @@
             $num = 0;
 
             for($i=0;$i<=100;$i++){
-                if($num % 2 == 0){
-                    echo "Fizz";
-                }
-                $num += 1
+                echo $num;
+                $num += 1;
             }
         
         ?>
