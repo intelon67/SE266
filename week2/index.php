d0773dbc972b661f94267845dc22eb7cc60395cc
intelon67 <ex0tic67@gmail.com>
2020-05-10 21:17:38 -0400

    modified:   index.php 	modified:   ../@

--- week2/index.php
+++ week2/index.php
@@ -1,10 +1,56 @@
+<!DOCTYPE html>
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '
- <h1>Test Site</h1>
- <p>Hello World</p>'; ?> 
- </body>
+    <head>
+        <meta charset="UTF-8">
+        <title>SE 266 - Week 2</title>
+    </head>
+    <style>
+    body {background-image:url('bg.jpg')}
+
+    .header {margin:auto;
+            width:600px;
+            height:40px;
+            background-color:black;}
+
+
+    footer {border-top-style:solid 1px;
+            margin:auto;
+            width:600px;
+            height:20px;
+            background-color:black;}
+            
+    .main {background-color:gray;
+            border-style:solid 2px;
+            margin:auto;
+            width:600px;
+            height:1000px;}
+</style>
+    <body>
+    <div class='header'>
+        <h1><a href= "index.php">SE 266 - Jake Desmarais</a></h1>
+    </div>
+    <div class='main'>
+
+       
+       <a href="https://github.com/intelon67/SE266" style="font-size:24px;">My Github Repo</a>
+
+
+        <h2>Week 2 Assignments</h2>
+        <ul>
+        <li><a href="practioner1.php">PHP Practitioner 1</a></li>
+        <li><a href="practitioner2.php">PHP Practitioner 2</a></li>
+        <li><a href="practitioner3.php">PHP Practitioner 3</a></li>
+        <li><a href="practitioner4.php">PHP Practitioner 4</a></li>
+        <li><a href="fizzbuzz.php">Fizz Buzz</a></li>
+        </ul>
+
+
+
+            
+    </div>
+
+    <footer>
+    <p>Website Modified: 5/9/2020 at 3:51PM EST</p>
+    </footer>
+    </body>
 </html>
