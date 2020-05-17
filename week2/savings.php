0f3a5308df506b93c92117e95daf8a59eba016b8
intelon67 <ex0tic67@gmail.com>
2020-05-17 14:36:09 -0400

    modified:   savings.php

--- week2/savings.php
+++ week2/savings.php
@@ -68,7 +68,7 @@ if (array_key_exists('subBtn',$_POST)){
         <input type="submit" name="subBtn">
         </form>
 
-        <p><?php echo $saved; ?></p>
+        <p><?php echo "Amount Saved: $saved"; ?></p>
 
 
 
