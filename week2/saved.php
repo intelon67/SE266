8629389ebe8d63390bd03c37aac8b0fac9644e30
intelon67 <ex0tic67@gmail.com>
2020-05-17 12:39:27 -0400

    modified:   saved.php

--- week2/saved.php
+++ week2/saved.php
@@ -33,14 +33,11 @@
     $saved = calculate_savings($currentAmount,$monthlyInvestment,$interestRate,$numMonths);
 
     function calculate_savings($currentAmount,$monthlyInvestment,$interestRate,$numMonths){
-        
-        for ($i = 0;$i <= $numMonths; $i++){
-        $monthlyInterest = $interestRate/$numMonths/$monthlyInvestment;
-        $monthlyIncrease = $currentAmount * (1+$monthlyInterest);
-        $saved = $monthlyInvestment + $monthlyIncrease;
+        for($i=0;$i<$numMonths;$i++){
+        $monthlyInterest = $interestRate/12/100;
+        $monthlyIncrease = ($currentAmount + $monthlyInvestment) * $monthlyInterest;
         }
-
-        return $saved;
+        return $monthlyIncrease;
     }
 
 
