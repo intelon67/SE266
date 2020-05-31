a3935b4634304ff690b696cb8dfef9a7fad3ef76
intelon67 <ex0tic67@gmail.com>
2020-05-31 16:05:55 -0400

    modified:   index.php

--- week4/index.php
+++ week4/index.php
@@ -4,7 +4,7 @@ $username = "se266_Jacob";
 $password = "Jacob";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=se266_Jacob", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=patients", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
