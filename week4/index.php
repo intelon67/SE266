707f6b7d2df1dfae4ce052518e68dfb00ba44a53
intelon67 <ex0tic67@gmail.com>
2020-05-31 16:02:28 -0400

    modified:   index.php

--- week4/index.php
+++ week4/index.php
@@ -1,19 +1,14 @@
 <?php
-$host = 'ict.neit.edu,5500';
-$db   = 'SE266_Jacob';
-$user = 'SE266_Jacob';
-$pass = 'Jacob';
-$charset = 'utf8mb4';
+$servername = "ict.neit.edu,5500";
+$username = "se266_Jacob";
+$password = "Jacob";
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
 try {
-     $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+  $conn = new PDO("mysql:host=$servername;dbname=se266_Jacob", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
 }
 ?>
